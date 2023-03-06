2ba55f4c2593dcd50f1cf5bdf269c77bf1c4ce7a
son109153006 <kymi20011018@gmail.com>
2023-03-06 15:18:17 +0800

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace son_work
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            txtShow.Text = "Hello World!"; //你可以直接複製貼過去
+        }
+    }
+}
